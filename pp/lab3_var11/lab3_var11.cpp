1f77192b50981d7ad9791aec163e698c5e2769ed
jabovik <mojabovik@gmail.com>
2025-10-31 22:05:25 +0400

    lab3pp: changed selection to insertion

--- pp/lab3_var11/lab3_var11.cpp
+++ pp/lab3_var11/lab3_var11.cpp
@@ -7,22 +7,21 @@
 #include <iomanip>
 using namespace std;
 
-/// @brief сортировка выбором
-/// @param vec сортируемый вектор
-void selection_sort(vector<int> &vec)
+/// @brief сортировка вставками
+/// @param vec 
+void insertion_sort(vector<int> &vec)
 {
     size_t size = vec.size();
-    for (size_t i = 0; i < size - 1; ++i)
+    for (size_t i = 1; i < size; ++i) // проходим по всем элементам, начиная со второго
     {
-        size_t min_idx = i;
-        for (size_t j = i + 1; j < size; ++j)
+        int key = vec[i]; // текущий элемент для вставки
+        size_t j = i;
+        while (j > 0 && vec[j - 1] > key) //
         {
-            if (vec[min_idx] > vec[j])
-            {
-                min_idx = j;
-            }
+            vec[j] = vec[j - 1]; // сдвигаем элемент вправо
+            --j;
         }
-        swap(vec[i], vec[min_idx]);
+        vec[j] = key; // вставляем текущий элемент на его место
     }
 }
 /// @brief функция слияния двух отсортированных частей
@@ -30,21 +29,21 @@ void selection_sort(vector<int> &vec)
 /// @param left начальный индекс
 /// @param right конечный индекс
 /// @param mid срединный индекс (конец левой части включительно)
-void merge(vector<int> &vec, size_t left, size_t right, size_t mid)
+void _merge(vector<int> &vec, size_t left, size_t right, size_t mid)
 {
-    vector<int> left_vec(mid - left + 1);
-    vector<int> right_vec(right - mid);
+    vector<int> left_vec(mid - left + 1); // левая часть
+    vector<int> right_vec(right - mid); // правая часть
     for (size_t i = 0; i < left_vec.size(); ++i)
-        left_vec[i] = vec[left + i];
+        left_vec[i] = vec[left + i]; // копируем элементы в левую часть
     for (size_t i = 0; i < right_vec.size(); ++i)
-        right_vec[i] = vec[mid + 1 + i];
+        right_vec[i] = vec[mid + 1 + i]; // копируем элементы в правую часть
 
     size_t i = 0, j = 0, k = left;
     while (i < left_vec.size() && j < right_vec.size()) // пока есть элементы в обеих частях
     {
         if (left_vec[i] <= right_vec[j]) // сравниваем элементы из обеих частей
         {
-            vec[k++] = left_vec[i++]; // добавляем меньший элемент в результирующий массив
+            vec[k++] = left_vec[i++]; // добавляем меньший элемент в результирующий вектор
         }
         else
         {
@@ -60,26 +59,32 @@ void merge(vector<int> &vec, size_t left, size_t right, size_t mid)
         vec[k++] = right_vec[j++];
     }
 }
-/// @brief рекурсивная функция сортировки слиянием
+/// @brief рекурсивная вспомогательная функция сортировки слиянием
 /// @param vec сортируемый вектор
 /// @param left начальный индекс
 /// @param right конечный индекс
-void merge_sort_recursive(vector<int> &vec, size_t left, size_t right)
+void _merge_sort_recursive(vector<int> &vec, size_t left, size_t right)
 {
-    if (left >= right)
+    if (left >= right) // базовый случай рекурсии (один элемент)
         return;
-    size_t mid = left + (right - left) / 2;
-    merge_sort_recursive(vec, left, mid);
-    merge_sort_recursive(vec, mid + 1, right);
-    merge(vec, left, right, mid);
+    size_t mid = left + (right - left) / 2; // разделение массива на две части
+    _merge_sort_recursive(vec, left, mid); // рекурсивная сортировка левой части
+    _merge_sort_recursive(vec, mid + 1, right); // рекурсивная сортировка правой части
+    _merge(vec, left, right, mid); // слияние двух отсортированных частей
 }
 /// @brief сортировка слиянием
 /// @param vec сортируемый вектор
 void merge_sort(vector<int> &vec)
 {
     size_t size = vec.size();
-    merge_sort_recursive(vec, 0, size - 1);
+    _merge_sort_recursive(vec, 0, size - 1); // запуск алгоритма
 }
+/// @brief шаблонная функция безопасного ввода
+/// @tparam T
+/// @param prompt пояснение пользователю
+/// @param error_message
+/// @param validator функция проверки корректности ввода
+/// @return обработанный и безопасный ввод
 template <typename T>
 T safe_input(const string &prompt, const string &error_message, function<bool(T)> validator = nullptr)
 {
@@ -145,22 +150,22 @@ void print_vector(const vector<int> &vec, size_t REASONABLE_SIZE = 100)
     }
     cout << endl;
 }
-
+/// @brief функция вывода длительности с правильным форматированием.
+/// @param duration
 void print_duration(uint64_t duration)
 {
     if (duration > 1'000'000)
     {
-        cout << "Duration: " << duration/1'000'000 << "ms\n";
+        cout << "Duration: " << duration / 1'000'000 << "ms\n";
     }
     else if (duration > 1'000)
     {
-        cout << "Duration: " << duration/1'000 << "us\n";
+        cout << "Duration: " << duration / 1'000 << "us\n";
     }
     else
     {
         cout << "Duration: " << duration << "ns\n";
     }
-    
 }
 
 int main(int argc, char const *argv[])
@@ -170,7 +175,7 @@ int main(int argc, char const *argv[])
     while (run)
     {
         char choice;
-        cout << "Bubble and Merge sort\n";
+        cout << "Insertion and Merge sort\n";
         int size = safe_input<int>("Input vector size:", "Invalid input", [](int s)
                                    { return s >= 1; });
         vector<int> vec(size);
@@ -201,19 +206,19 @@ int main(int argc, char const *argv[])
                 vec = generate_almost_sorted_vector(size);
             }
         }
-        vector<int> vec_selection = vec;
+        vector<int> vec_insertion = vec;
         vector<int> vec_merge = vec;
         vector<int> vec_stdsort = vec;
         cout << "Original vector:\n";
         print_vector(vec);
 
         auto t1_s = chrono::high_resolution_clock::now();
-        selection_sort(vec_selection);
+        insertion_sort(vec_insertion);
         auto t1_e = chrono::high_resolution_clock::now();
         auto t1_d = chrono::duration_cast<chrono::nanoseconds>(t1_e - t1_s).count();
 
-        cout << "Selection sort:\n";
-        print_vector(vec_selection);
+        cout << "Insertion sort:\n";
+        print_vector(vec_insertion);
         print_duration(t1_d);
 
         auto t2_s = chrono::high_resolution_clock::now();
@@ -234,21 +239,21 @@ int main(int argc, char const *argv[])
         print_vector(vec_stdsort);
         print_duration(t3_d);
 
-        long double stdsort_selection_ratio = static_cast<long double>(t1_d) / static_cast<long double>(t3_d);
-        string stdsort_selection_relation = "faster";
-        if(stdsort_selection_ratio<1)
+        long double stdsort_insertion_ratio = static_cast<long double>(t1_d) / static_cast<long double>(t3_d);
+        string stdsort_insertion_relation = "faster";
+        if (stdsort_insertion_ratio < 1)
         {
-            stdsort_selection_ratio = 1/stdsort_selection_ratio;
-            stdsort_selection_relation = "slower";
+            stdsort_insertion_ratio = 1 / stdsort_insertion_ratio;
+            stdsort_insertion_relation = "slower";
         }
         long double stdsort_merge_ratio = static_cast<long double>(t2_d) / static_cast<long double>(t3_d);
         string stdsort_merge_relation = "faster";
-        if(stdsort_merge_ratio<1)
+        if (stdsort_merge_ratio < 1)
         {
-            stdsort_merge_ratio = 1/stdsort_merge_ratio;
+            stdsort_merge_ratio = 1 / stdsort_merge_ratio;
             stdsort_merge_relation = "slower";
         }
-        cout << "std::sort() is " << setprecision(3) << stdsort_selection_ratio << " times " << stdsort_selection_relation << " than selection sort\n";
+        cout << "std::sort() is " << setprecision(3) << stdsort_insertion_ratio << " times " << stdsort_insertion_relation << " than insertion sort\n";
         cout << "std::sort() is " << setprecision(3) << stdsort_merge_ratio << " times " << stdsort_merge_relation << " than merge sort\n";
         choice = safe_input<char>("Do you want to continue? (y/n):", "(y/n)", [](char c)
                                   { return string("YyNn").find(c) != string::npos; });
