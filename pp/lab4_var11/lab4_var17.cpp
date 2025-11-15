b73be0429077ed70e79724f95d28c0df890358cc
jabovik <mojabovik@gmail.com>
2025-11-15 07:52:08 +0400

    lab3pp:done

--- pp/lab4_var11/lab4_var17.cpp
+++ pp/lab4_var11/lab4_var17.cpp
@@ -6,33 +6,37 @@
 #include <string>
 using namespace std;
 
+/// @brief функция поиска индексов начала слов, длина которых меньше заданной
+/// @param str строка
+/// @param length длина слова
+/// @return вектор индексов начала слов
 vector<size_t> find_words_w_less_len(const string &str, size_t length)
 {
     vector<size_t> indices;
     size_t count = 0;
     for (size_t i = 0; i < str.size(); i++)
     {
-        if (!isspace(str[i]) && !ispunct(str[i]))
+        if (isalnum(str[i]))
             ++count;
         else
         {
-            if (count < length)
+            if (count < length && count != 0)
             {
                 indices.push_back(i - count);
             }
             count = 0;
         }
     }
-    if (count < length)
+    if (count < length && count != 0)
     {
         indices.push_back(str.size() - count);
     }
     return indices;
 }
 
-void replace_letters(string &str, size_t length, char replacement)
+void replace_letters(string &str, const vector<size_t> &indices, char replacement)
 {
-    for (auto idx : find_words_w_less_len(str, length))
+    for (auto idx : indices)
     {
         str[idx] = replacement;
     }
@@ -46,9 +50,8 @@ int main(int argc, char const *argv[])
     size_t length;
     getline(input, str);
     input >> length;
-    replace_letters(str, length, '*');
+    vector<size_t> indices = find_words_w_less_len(str, length);
+    replace_letters(str,indices, '*');
     output << str;
-    input.close();
-    output.close();
     return 0;
 }
