a24b93a0177c3deffa0aac786d50532603099c50
jabovik <mojabovik@gmail.com>
2026-03-10 16:53:42 +0400

    up

--- sem2/YAPISD/lab1_var22/lab1_var22.cpp
+++ sem2/YAPISD/lab1_var22/lab1_var22.cpp
@@ -1,6 +1,6 @@
-
 #include <iomanip>
 #include <fstream>
+#include <cmath>
 double foo(double* arr, int size, double d)
 {
     double cumsum = 0;
@@ -21,15 +21,32 @@ int main(int argc, char const *argv[])
     std::ofstream output("output.txt");
     double d;
     input >> d;
-    double arr[1000];
     double el;
     int size = 0;
+    /*
+    double arr[1000];
     while(input >> el)
     {
         arr[size] = el;
         ++size;
     }
+    */
+    while(input >> el) // подсчет кол-ва элементов
+    {
+        ++size;
+    }
+    input.clear();
+    input.seekg(0); // возврат к началу файла
+    input >> el; // пропуск первой строки
+    double *arr = new double[size];
+    int i = 0;
+    while(input >> el)
+    {
+        arr[i] = el;
+        ++i;
+    }
     double cumsum = foo(arr, size, d);
-    output << std::setprecision(15) << cumsum;
+    output << std::setprecision(15) << cumsum; // форматирование под условия тестов
+    delete[] arr;
     return 0;
 }
