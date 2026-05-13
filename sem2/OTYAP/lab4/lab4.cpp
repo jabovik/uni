69205cf71f8121941f64574c190c85e999ddbbfd
jabovik <mojabovik@gmail.com>
2026-05-13 17:04:25 +0400

    lab4 works

--- sem2/OTYAP/lab4/lab4.cpp
+++ sem2/OTYAP/lab4/lab4.cpp
@@ -13,22 +13,22 @@ char *read_file(std::ifstream &input, size_t &size)
     return buffer;
 }
 
-enum ELexType
+enum ELexType // отсортированы в лексикографическом порядке
 {
+    AO,
+    CO,
+    DO, // do
+    EN, // end
+    EQ,
     ID,
     KW,
-    DO, // do
-    LP, // loop
-    UN, // until
-    EN, // end
-    NO, // not
     LO, // and/or
-    CO,
-    EQ,
-    AO,
+    LP, // loop
+    NO, // not
+    SC,
+    UN, // until
     VL,
     WL,
-    SC,
 };
 const int LEX_TYPES_NUM = 14;
 
@@ -40,6 +40,34 @@ struct Lex
     Lex *next;
     Lex *prev;
 };
+
+// функция push для двусвязного нециклического списка лексем
+Lex *push(Lex *node, Lex *new_node)
+{
+    new_node->next = nullptr;
+    new_node->prev = nullptr;
+    if (node)
+    {
+        if (node->next)
+        {
+            node->next->prev = new_node;
+            new_node->next = node->next;
+        }
+        node->next = new_node;
+        new_node->prev = node;
+    }
+    return new_node;
+}
+
+Lex *push(Lex *node, char *str, ELexType type, int pos)
+{
+    Lex *new_node = new Lex;
+    new_node->str = new char[strlen(str) + 1];
+    strcpy(new_node->str, str);
+    new_node->type = type;
+    new_node->pos = pos;
+    return push(node, new_node);
+}
 
 namespace Lexical
 {
@@ -142,34 +170,6 @@ namespace Lexical
         default:
             return "wl";
         }
-    }
-
-    // функция push для двусвязного нециклического списка лексем
-    Lex *push(Lex *node, Lex *new_node)
-    {
-        new_node->next = nullptr;
-        new_node->prev = nullptr;
-        if (node)
-        {
-            if (node->next)
-            {
-                node->next->prev = new_node;
-                new_node->next = node->next;
-            }
-            node->next = new_node;
-            new_node->prev = node;
-        }
-        return new_node;
-    }
-
-    Lex *push(Lex *node, char *str, ELexType type, int pos)
-    {
-        Lex *new_node = new Lex;
-        new_node->str = new char[strlen(str) + 1];
-        strcpy(new_node->str, str);
-        new_node->type = type;
-        new_node->pos = pos;
-        return push(node, new_node);
     }
 
     ELexType det_keyword(const char *str)
@@ -385,24 +385,233 @@ namespace Lexical
 
 namespace Syntaxical
 {
-    const int STATES_NUM = 1; ///////////////////////////////////////////////////////
+    const int STATES_NUM = 11; ///////////////////////////////////////////////////////
     enum EState
     {
         S_INIT,
-        S_FINISH,
+        S_POSTDO,
+        S_POSTID,
+        S_OPER_OR_TAIL,
+        S_ARITH_TAIL,
+        S_POST_LOOP,
+        S_POST_UNTIL,
+        S_LOGIC_OPER,
+        S_LOGIC_TAIL, // конечное состояние
+        S_COMPARE_TAIL,
+        S_ERROR,
     };
-
-    void create_table(EState table[LEX_TYPES_NUM][STATES_NUM][LEX_TYPES_NUM]) // лексема(сигнал), текущее состояние, стек
+    // стек имеет вид LOGICAL, UNTIL, LOOP, OPERATOR, [do]
+    void create_table(EState table[LEX_TYPES_NUM][STATES_NUM]) // лексема(сигнал), текущее состояние
     {
-        table[DO][S_INIT][];
+        table[KW][S_INIT] = S_ERROR;
+        table[ID][S_INIT] = S_ERROR; // ?
+        table[DO][S_INIT] = S_POSTDO;
+        table[LP][S_INIT] = S_ERROR;
+        table[UN][S_INIT] = S_ERROR;
+        table[EN][S_INIT] = S_ERROR;
+        table[NO][S_INIT] = S_ERROR;
+        table[LO][S_INIT] = S_ERROR;
+        table[CO][S_INIT] = S_ERROR;
+        table[EQ][S_INIT] = S_ERROR;
+        table[AO][S_INIT] = S_ERROR;
+        table[VL][S_INIT] = S_ERROR;
+        table[WL][S_INIT] = S_ERROR;
+        table[SC][S_INIT] = S_ERROR;
+
+        table[KW][S_POSTDO] = S_ERROR;
+        table[ID][S_POSTDO] = S_POSTID;
+        table[DO][S_POSTDO] = S_ERROR;
+        table[LP][S_POSTDO] = S_ERROR;
+        table[UN][S_POSTDO] = S_ERROR;
+        table[EN][S_POSTDO] = S_ERROR;
+        table[NO][S_POSTDO] = S_ERROR;
+        table[LO][S_POSTDO] = S_ERROR;
+        table[CO][S_POSTDO] = S_ERROR;
+        table[EQ][S_POSTDO] = S_ERROR;
+        table[AO][S_POSTDO] = S_ERROR;
+        table[VL][S_POSTDO] = S_ERROR;
+        table[WL][S_POSTDO] = S_ERROR;
+        table[SC][S_POSTDO] = S_ERROR;
+
+        table[KW][S_POSTID] = S_ERROR;
+        table[ID][S_POSTID] = S_ERROR;
+        table[DO][S_POSTID] = S_ERROR;
+        table[LP][S_POSTID] = S_ERROR;
+        table[UN][S_POSTID] = S_ERROR;
+        table[EN][S_POSTID] = S_ERROR;
+        table[NO][S_POSTID] = S_ERROR;
+        table[LO][S_POSTID] = S_ERROR;
+        table[CO][S_POSTID] = S_ERROR;
+        table[EQ][S_POSTID] = S_OPER_OR_TAIL;
+        table[AO][S_POSTID] = S_ERROR;
+        table[VL][S_POSTID] = S_ERROR;
+        table[WL][S_POSTID] = S_ERROR;
+        table[SC][S_POSTID] = S_ERROR;
+
+        table[KW][S_OPER_OR_TAIL] = S_ERROR;
+        table[ID][S_OPER_OR_TAIL] = S_ARITH_TAIL;
+        table[DO][S_OPER_OR_TAIL] = S_ERROR;
+        table[LP][S_OPER_OR_TAIL] = S_ERROR;
+        table[UN][S_OPER_OR_TAIL] = S_ERROR;
+        table[EN][S_OPER_OR_TAIL] = S_ERROR;
+        table[NO][S_OPER_OR_TAIL] = S_ERROR;
+        table[LO][S_OPER_OR_TAIL] = S_ERROR;
+        table[CO][S_OPER_OR_TAIL] = S_ERROR;
+        table[EQ][S_OPER_OR_TAIL] = S_ERROR;
+        table[AO][S_OPER_OR_TAIL] = S_ERROR;
+        table[VL][S_OPER_OR_TAIL] = S_ARITH_TAIL;
+        table[WL][S_OPER_OR_TAIL] = S_ERROR;
+        table[SC][S_OPER_OR_TAIL] = S_ERROR;
+
+        table[KW][S_ARITH_TAIL] = S_ERROR;
+        table[ID][S_ARITH_TAIL] = S_ERROR;
+        table[DO][S_ARITH_TAIL] = S_ERROR;
+        table[LP][S_ARITH_TAIL] = S_POST_LOOP;
+        table[UN][S_ARITH_TAIL] = S_ERROR;
+        table[EN][S_ARITH_TAIL] = S_ERROR;
+        table[NO][S_ARITH_TAIL] = S_ERROR;
+        table[LO][S_ARITH_TAIL] = S_ERROR;
+        table[CO][S_ARITH_TAIL] = S_ERROR;
+        table[EQ][S_ARITH_TAIL] = S_ERROR;
+        table[AO][S_ARITH_TAIL] = S_OPER_OR_TAIL;
+        table[VL][S_ARITH_TAIL] = S_ERROR;
+        table[WL][S_ARITH_TAIL] = S_ERROR;
+        table[SC][S_ARITH_TAIL] = S_POSTDO;
+
+        table[KW][S_POST_LOOP] = S_ERROR;
+        table[ID][S_POST_LOOP] = S_ERROR;
+        table[DO][S_POST_LOOP] = S_ERROR;
+        table[LP][S_POST_LOOP] = S_ERROR;
+        table[UN][S_POST_LOOP] = S_POST_UNTIL;
+        table[EN][S_POST_LOOP] = S_ERROR;
+        table[NO][S_POST_LOOP] = S_ERROR;
+        table[LO][S_POST_LOOP] = S_ERROR;
+        table[CO][S_POST_LOOP] = S_ERROR;
+        table[EQ][S_POST_LOOP] = S_ERROR;
+        table[AO][S_POST_LOOP] = S_ERROR;
+        table[VL][S_POST_LOOP] = S_ERROR;
+        table[WL][S_POST_LOOP] = S_ERROR;
+        table[SC][S_POST_LOOP] = S_ERROR;
+
+        table[KW][S_POST_UNTIL] = S_ERROR;
+        table[ID][S_POST_UNTIL] = S_LOGIC_TAIL;
+        table[DO][S_POST_UNTIL] = S_ERROR;
+        table[LP][S_POST_UNTIL] = S_ERROR;
+        table[UN][S_POST_UNTIL] = S_ERROR;
+        table[EN][S_POST_UNTIL] = S_ERROR;
+        table[NO][S_POST_UNTIL] = S_LOGIC_OPER;
+        table[LO][S_POST_UNTIL] = S_ERROR;
+        table[CO][S_POST_UNTIL] = S_ERROR;
+        table[EQ][S_POST_UNTIL] = S_ERROR;
+        table[AO][S_POST_UNTIL] = S_ERROR;
+        table[VL][S_POST_UNTIL] = S_LOGIC_TAIL;
+        table[WL][S_POST_UNTIL] = S_ERROR;
+        table[SC][S_POST_UNTIL] = S_ERROR;
+
+        table[KW][S_LOGIC_OPER] = S_ERROR;
+        table[ID][S_LOGIC_OPER] = S_LOGIC_TAIL;
+        table[DO][S_LOGIC_OPER] = S_ERROR;
+        table[LP][S_LOGIC_OPER] = S_ERROR;
+        table[UN][S_LOGIC_OPER] = S_ERROR;
+        table[EN][S_LOGIC_OPER] = S_ERROR;
+        table[NO][S_LOGIC_OPER] = S_ERROR;
+        table[LO][S_LOGIC_OPER] = S_ERROR;
+        table[CO][S_LOGIC_OPER] = S_ERROR;
+        table[EQ][S_LOGIC_OPER] = S_ERROR;
+        table[AO][S_LOGIC_OPER] = S_ERROR;
+        table[VL][S_LOGIC_OPER] = S_LOGIC_TAIL;
+        table[WL][S_LOGIC_OPER] = S_ERROR;
+        table[SC][S_LOGIC_OPER] = S_ERROR;
+
+        table[KW][S_LOGIC_TAIL] = S_ERROR;
+        table[ID][S_LOGIC_TAIL] = S_ERROR;
+        table[DO][S_LOGIC_TAIL] = S_POSTDO;
+        table[LP][S_LOGIC_TAIL] = S_ERROR;
+        table[UN][S_LOGIC_TAIL] = S_ERROR;
+        table[EN][S_LOGIC_TAIL] = S_ERROR;
+        table[NO][S_LOGIC_TAIL] = S_ERROR;
+        table[LO][S_LOGIC_TAIL] = S_POST_UNTIL;
+        table[CO][S_LOGIC_TAIL] = S_COMPARE_TAIL;
+        table[EQ][S_LOGIC_TAIL] = S_COMPARE_TAIL;
+        table[AO][S_LOGIC_TAIL] = S_ERROR;
+        table[VL][S_LOGIC_TAIL] = S_ERROR;
+        table[WL][S_LOGIC_TAIL] = S_ERROR;
+        table[SC][S_LOGIC_TAIL] = S_ERROR;
+
+        table[KW][S_COMPARE_TAIL] = S_ERROR;
+        table[ID][S_COMPARE_TAIL] = S_LOGIC_TAIL;
+        table[DO][S_COMPARE_TAIL] = S_ERROR;
+        table[LP][S_COMPARE_TAIL] = S_ERROR;
+        table[UN][S_COMPARE_TAIL] = S_ERROR;
+        table[EN][S_COMPARE_TAIL] = S_ERROR;
+        table[NO][S_COMPARE_TAIL] = S_ERROR;
+        table[LO][S_COMPARE_TAIL] = S_ERROR;
+        table[CO][S_COMPARE_TAIL] = S_ERROR;
+        table[EQ][S_COMPARE_TAIL] = S_ERROR;
+        table[AO][S_COMPARE_TAIL] = S_ERROR;
+        table[VL][S_COMPARE_TAIL] = S_LOGIC_TAIL;
+        table[WL][S_COMPARE_TAIL] = S_ERROR;
+        table[SC][S_COMPARE_TAIL] = S_ERROR;
+
+        table[KW][S_ERROR] = S_ERROR;
+        table[ID][S_ERROR] = S_ERROR;
+        table[DO][S_ERROR] = S_ERROR;
+        table[LP][S_ERROR] = S_ERROR;
+        table[UN][S_ERROR] = S_ERROR;
+        table[EN][S_ERROR] = S_ERROR;
+        table[NO][S_ERROR] = S_ERROR;
+        table[LO][S_ERROR] = S_ERROR;
+        table[CO][S_ERROR] = S_ERROR;
+        table[EQ][S_ERROR] = S_ERROR;
+        table[AO][S_ERROR] = S_ERROR;
+        table[VL][S_ERROR] = S_ERROR;
+        table[WL][S_ERROR] = S_ERROR;
+        table[SC][S_ERROR] = S_ERROR;
     }
 
-    void SynAnalysis(Lex *lex_list)
+    void print_expected(EState table[LEX_TYPES_NUM][STATES_NUM], EState prev_state, std::ofstream &output, int pos)
+    {
+        output << pos << ' ';
+        bool first = true;
+        for (int i = 0; i < LEX_TYPES_NUM; ++i)
+        {
+            if (table[i][prev_state] != S_ERROR)
+            {
+                if (!first)
+                    output << ' ';
+                first = false;
+                output << Lexical::lex_type_to_str((ELexType)i);
+            }
+        }
+    }
+
+    void SynAnalysis(Lex *lex_list, std::ofstream &output)
     {
         EState state = S_INIT;
-        while (lex_list)
+        EState prev_state;
+        ELexType signal;
+        EState table[LEX_TYPES_NUM][STATES_NUM];
+        create_table(table);
+        int pos = 0;
+        while (state != S_ERROR && lex_list)
         {
+            prev_state = state;
+            signal = lex_list->type;
+            state = table[signal][state];
+            if (state == S_ERROR)
+            {
+                print_expected(table, prev_state, output, pos);
+            }
             lex_list = lex_list->next;
+            pos++;
+        }
+        if (state == S_LOGIC_TAIL)
+        {
+            output << "OK";
+        }
+        else if (state != S_ERROR)
+        {
+            print_expected(table, state, output, pos);
         }
     }
 }
@@ -416,16 +625,13 @@ int main(int argc, char const *argv[])
     char *text = read_file(input, size);
     input.close();
     Lex *lex_list = Lexical::lexAnalysis(text, size);
+    std::ofstream output("output.txt");
     if (lex_list)
     {
-        std::ofstream output("output.txt");
         Lexical::print_list(lex_list, output);
-        output << std::endl;
-        Lexical::print_list(lex_list, output, ID);
-        output << std::endl;
-        Lexical::print_list(lex_list, output, VL);
-        output.close();
     }
+    output << std::endl;
+    Syntaxical::SynAnalysis(lex_list, output);
     delete[] text;
     Lexical::delete_list(lex_list);
     return 0;
